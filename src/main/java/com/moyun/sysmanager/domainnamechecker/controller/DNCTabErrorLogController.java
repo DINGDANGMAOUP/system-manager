69b3c0a0b42dcfb49394e25cff399e3877bd6911
kueoneko <964621334@qq.com>
2020-08-11 23:16:13 +0800

    done

--- src/main/java/com/moyun/sysmanager/domainnamechecker/controller/DNCTabErrorLogController.java
+++ src/main/java/com/moyun/sysmanager/domainnamechecker/controller/DNCTabErrorLogController.java
@@ -1,15 +1,15 @@
-//package com.moyun.sysmanager.domainnamechecker.controller;
-//
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * @author kuroneko
-// */
-//@RestController
-//@RequestMapping("domain/te")
-//public class DNCTabErrorLogController {
-//
-//}
+package com.moyun.sysmanager.domainnamechecker.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kuroneko
+ */
+@RestController
+@RequestMapping("domain/te")
+public class DNCTabErrorLogController {
+
+}
